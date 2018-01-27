54bf33a3c428213b9fbed5b7daa42b6fde253e63
Isadora Barros <isadora.barros@enernoc.com>
2018-01-26 21:51:43 -0300

    Add Card component with lorem ipsum text

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const style = {
+      backgroundColor: '#D3D3D3',
+      fontFamily: 'sans-serif',
+      fontWeight: 'bold',
+    };
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Today I Learned</h1>
-        </header>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Today I Learned</h1>
+          </header>
+          <Card>
+            <CardHeader
+              title="How to install node"
+              style={style}
+            />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+          <Card>
+            <CardHeader
+              title="Remove directories recursively"
+              style={style}
+            />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
